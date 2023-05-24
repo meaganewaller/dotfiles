e7ec518f5a55a6e301d0c2b77e0324e6350ff3b7
Meagan Waller <meagan.waller@testdouble.com>
2023-05-24 12:07:53 -0400

    [feat: fish] Add fish shell configuration

---
+++ shells/fish/functions/grestart.fish
@@ -0,0 +1,5 @@
+function grestart
+    git checkout main; or git checkout development
+    git pull
+    git-trim --no-confirm
+end
---
+++ shells/fish/functions/nvim.fish
@@ -0,0 +1,15 @@
+function nvim
+	set -l NVIM (which nvim)
+
+	if test (count $argv) -lt 1
+		"$NVIM"
+		return
+	end
+
+	if test -d $argv[1]
+		pushd $argv[1] && "$NVIM" && popd
+		return
+	end
+
+	"$NVIM" $argv
+end
