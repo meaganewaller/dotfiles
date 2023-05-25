97b7ce6f5ae77f6c4b1f1208224387b151c90b11
Meagan Waller <meagan.waller@testdouble.com>
2023-05-25 11:56:24 -0400

    [feat: nvim] Add neotest config

--- editors/nvim/lua/meg/plugins/emmet.lua
+++ editors/nvim/lua/meg/plugins/emmet.lua
@@ -1,8 +1,14 @@
-vim.g.user_emmet_mode = 'i'
+vim.g.user_emmet_mode = "i"
 vim.g.user_emmet_install_global = 0
 vim.g.user_emmet_install_command = 0
 vim.g.user_emmet_complete_tag = 0
 
-nx.au({
-  { "<FileType>", pattern = { "html", "css", "vue", "javascript", "javascriptreact", "svelte", "typescript", "typescriptreact" }}, command = function() vim.cmd [[ EmmetInstall imap <silent><buffer> <C-y> <Plug>(emmet-expand-abbr) ]]
-})
+-- nx.au({
+-- 	{
+-- 		"<FileType>",
+-- 		pattern = { "html", "css", "vue", "javascript", "javascriptreact", "svelte", "typescript", "typescriptreact" },
+-- 		command = function()
+-- 			vim.cmd([[ EmmetInstall imap <silent><buffer> <C-y> <Plug>(emmet-expand-abbr) ]])
+-- 		end,
+-- 	},
+-- })
