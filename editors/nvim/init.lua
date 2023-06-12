39e0a4a908a6230a890307c2ce0399264e140300
Meagan Waller <meagan.waller@testdouble.com>
2023-06-12 10:29:52 -0400

    :sparkles: Add neotest plugins and decay scheme

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -1,18 +1,19 @@
 -- bootstrap lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- set leader to space
+-- set localleader to comma
 -- this needs to occur before initializing lazy
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
@@ -20,36 +21,3 @@ vim.g.maplocalleader = ","
 _G.mw = {}
 
 require("meg")
-
--- require("lazy").setup("meg.plugins", {
---   debug = false,
---   defaults = { lazy = true },
---   checker = { enabled = false },
---   diff = {
---     cmd = "terminal_git",
---   },
---   rtp = {
---     disabled_plugins = {
---       "gzip",
---       "zip",
---       "zipPlugin",
---       "tar",
---       "tarPlugin",
---       "getscript",
---       "getscriptPlugin",
---       "vimball",
---       "vimballPlugin",
---       "2html_plugin",
---       "logipat",
---       "rrhelper",
---       "spellfile_plugin",
---       "matchit",
---       "tutor_mode_plugin",
---       "remote_plugins",
---       "shada_plugin",
---       "filetype",
---       "spellfile",
---       "tohtml",
---     },
---   },
--- })
--- editors/nvim/lua/meg/colorschemes/init.lua
+++ editors/nvim/lua/meg/colorschemes/init.lua
@@ -7,7 +7,9 @@ vim.cmd("colorscheme " .. default_colorscheme)
 ---Set kitty colorscheme based on neovim colorscheme.
 ---@param colors string @kitty colorscheme file
 local function kitty_colors(colors)
-	if not vim.fn.executable("kitty") then return end
+	if not vim.fn.executable("kitty") then
+		return
+	end
 	local cmd = "kitty @ --to %s set-colors -a %s"
 	local socket = vim.fn.expand("$KITTY_LISTEN_ON")
 	vim.fn.system(cmd:format(socket, "~/.config/kitty/" .. colors .. ".conf"))
@@ -22,14 +24,16 @@ local function set_hl()
 	})
 
 	if vim.g.colors_name == "rose-pine" then
-		kitty_colors("dracula")
+		-- kitty_colors("rose-pine")
 		require("meg.colorschemes.rose-pine").set_hl()
+	elseif vim.g.colors_name == "decay" then
+		require("meg.colorschemes.decay").set_hl()
 	elseif vim.g.colors_name == "tokyonight" then
-		kitty_colors("tokyonight_storm")
+		-- kitty_colors("tokyonight_storm")
 		require("meg.colorschemes.tokyonight").set_hl()
 	end
 
---	require("meg.lsp.plugins.lspsaga").set_hl()
+	--	require("meg.lsp.plugins.lspsaga").set_hl()
 end
 
 nx.au({ { "UIEnter", "Colorscheme" }, callback = set_hl })
