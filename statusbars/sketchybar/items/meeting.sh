c0acb3b6d71d64ff3252a51166771886b4af94e6
Meagan Waller <meagan.waller@testdouble.com>
2023-06-29 15:36:45 -0400

    :sparkles: Add meeting bar to sketchybar

--- statusbars/sketchybar/items/meeting.sh
+++ statusbars/sketchybar/items/meeting.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
 
 sketchybar --add alias "MeetingBar,Item-0" right \
-           --set "MeetingBar,Item-0" label.font="$ICON_FONT:Regular:12.0" \
+           --set "MeetingBar,Item-0" label.font="$ICON_FONT:Regular:12.0" associated_display=2 \
            --rename "MeetingBar,Item-0" meeting.alias \
            --set meeting.alias icon.drawing=off \
                                background.padding_right=0 \
                                align=right \
                                # click_script="$PLUGIN_DIR/meeting_click.sh" \
                                # script="$PLUGIN_DIR/meeting.sh" \
-                               popup.height=30 \
                                update_freq=1 \
-                               associated_display=1 \
                                font="$ICON_FONT:Regular:12.0" \
                                alias.scale=0.75
-           # --subscribe meeting.alias mouse.entered \
-           #                           mouse.exited \
-           #                           mouse.exited.global \
-           # --add item meeting.details popup.meeting.alias \
-           # --set meeting.details background.corner_radius=12 \
-           #                       background.padding_left=5 \
-           #                       background.padding_right=10 \
-           #                       click_script="sketchybar --set meeting.alias popup.drawing=off"
