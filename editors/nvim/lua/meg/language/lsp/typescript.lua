b8fc353b666a10844bfa03a7a8910dca57a8f0d3
Meagan Waller <meagan.waller@testdouble.com>
2023-08-22 12:12:38 -0400

    Typescript lsp fixes, set hardhacker theme

--- editors/nvim/lua/meg/language/lsp/typescript.lua
+++ editors/nvim/lua/meg/language/lsp/typescript.lua
@@ -1,15 +1,40 @@
 local M = {}
+local baseDefinitionHandler = vim.lsp.handlers["textDocument/definition"]
+local filter = require("meg.utils.lsp").filter
+local filterReactDTS = require("meg.utils.lsp").filterReactDTS
+
+local handlers = {
+  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    silent = true,
+    border = "rounded",
+  }),
+  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+  ["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    { virtual_text = true }
+  ),
+  ["textDocument/definition"] = function(err, result, method, ...)
+    P(result)
+    if vim.tbl_islist(result) and #result > 1 then
+      local filtered_result = filter(result, filterReactDTS)
+      return baseDefinitionHandler(err, filtered_result, method, ...)
+    end
+
+    baseDefinitionHandler(err, result, method, ...)
+  end,
+}
 
 function M.setup(_, on_attach)
   require("typescript-tools").setup({
     on_attach = on_attach,
+    handlers = handlers,
     settings = {
       separate_diagnostic_server = true,
-      composite_mode = "separate_diagnostic",
-      publish_diagnostic_on = "insert_leave",
-      tsserver_logs = "verbose",
-      code_lens = "all",
-      disable_member_code_lens = true,
+      tsserver_file_preferences = {
+        includeInlayParameterNameHints = "all",
+        includeCompletionsForModuleExports = true,
+        quotePreference = "auto",
+      },
     },
   })
 end
--- editors/nvim/lua/meg/settings.lua
+++ editors/nvim/lua/meg/settings.lua
@@ -22,13 +22,14 @@ function M.setup()
 
   -- options
   options.set(option, "clipboard", "unnamed,unnamedplus")
+  options.set(option, "title", true)
 
   options.set(option, "swapfile", false)
   options.set(option, "backup", false)
   options.set(option, "writebackup", false)
   options.set(option, "undodir", vim.fn.stdpath "data" .. "/undodir")
 
-  options.set(option, "colorcolumn", 100)
+  options.set(option, "colorcolumn", "100")
   options.set(option, "updatetime", 50)
   options.set(option, "hidden", true)
   options.set(option, "ignorecase", true)
@@ -39,7 +40,12 @@ function M.setup()
   options.set(option, "splitright", true)
   options.set(option, "cursorline", true)
   options.set(option, "pumheight", 10)
+  options.set(option, "inccommand", "split")
   options.set(option, "termguicolors", true)
+  options.set(option, "hlsearch", true)
+  options.set(option, "scrolloff", 10)
+  options.set(option, "shell", "fish")
+
 
   options.set(window, "number", true)
   options.set(window, "relativenumber", true)
@@ -47,16 +53,32 @@ function M.setup()
   options.set(option, "cmdheight", 2)
 
   options.set(buffer, "expandtab", true)
+  options.set(option, "smarttab", true)
+  options.set(option, "breakindent", true)
   options.set(buffer, "shiftwidth", 2)
   options.set(buffer, "softtabstop", 2)
+  options.set(option, "wrap", false)
   options.set(buffer, "tabstop", 2)
-  options.set(buffer, "autoindent", false)
+  options.set(buffer, "autoindent", true)
   options.set(buffer, "cindent", false)
-  options.set(buffer, "smartindent", false)
+  options.set(buffer, "smartindent", true)
 
   vim.api.nvim_create_user_command("Wqa", "wa | qa", { force = true })
   vim.cmd "cabbrev wqa Wqa"
   vim.cmd "cabbrev bda 1,$bd"
+
+  -- Undercurl
+  vim.cmd([[let &t_Cs = "\e[4:3m"]])
+  vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+  -- Turn off paste mode when leaving insert
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    pattern = '*',
+    command = "set nopaste"
+  })
+
+  -- Add asterisks in block comments
+  vim.opt.formatoptions:append { 'r' }
 end
 
 return M
