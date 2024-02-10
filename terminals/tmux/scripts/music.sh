1bbbafc0e47631aadb2e690d676c33a00c570139
Meagan Waller <meagan.waller@testdouble.com>
2024-02-10 13:06:05 -0500

    :art: Modify tmux config, try a TS inspired colorscheme lol

---
+++ terminals/tmux/scripts/music.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function display () {
+  spotify_status=$(spotify status | head -n 1)
+
+  if [[ $spotify_status == *"playing"* ]]; then
+    echo $(getName)
+  else
+    echo ""
+  fi
+}
+
+function getArtistName () {
+  spotify status artist
+}
+
+function getTrackName () {
+  spotify status track
+}
+
+function getName() {
+  local artist=$(getArtistName)
+  local song=$(getTrackName)
+  echo "${artist} - ${song}"
+}
+
+display
--- terminals/tmux/scripts/uptime.sh
+++ terminals/tmux/scripts/uptime.sh
@@ -1,3 +1,2 @@
-#!/usr/local/bin/bash
-# uptime | awk '{print $3, $5}' | sed "s/:/ /g" | sed "s/,//g" | awk '{print $1"d", $2"h", $3"m"}'
-uptime | awk '{print $3, $5}' | sed "s/:/ /g" | sed "s/,//g" | sed "s/ /:/mod +x "
+#!/bin/bash
+uptime | rev | cut -d":" -f1 | rev | sed s/,//g
