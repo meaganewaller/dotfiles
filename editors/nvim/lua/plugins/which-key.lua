da73332a23a07e5ea5d993c347a363677c234283
Meagan Waller <meagan.waller@gusto.com>
2024-10-14 17:32:39 -0400

    Add to which-key UI config

--- editors/nvim/lua/plugins/which-key.lua
+++ editors/nvim/lua/plugins/which-key.lua
@@ -1,14 +1,22 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
+  enabled = os.getenv('SSH_TTY') == nil,
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
   opts = {
+    preset = 'modern',
     win = {
+      no_overlap = false,
       border = 'double',
       title = false,
+    },
+    layout = {
+      width = { max = 80 },
+      spacing = 3,
+      align = 'left',
     },
     icons = {
       mappings = false,
