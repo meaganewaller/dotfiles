12bad34206e0c7316fd29c0a3a3c2cbc04ed1f73
Meagan Waller <meagan.waller@gusto.com>
2024-10-17 14:25:21 -0400

    Add configuration to which-key plugin

--- editors/nvim/lua/plugins/which-key.lua
+++ editors/nvim/lua/plugins/which-key.lua
@@ -2,11 +2,29 @@ return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
   enabled = os.getenv('SSH_TTY') == nil,
+  pin = true,
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
   opts = {
+    plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = true,
+        windows = false,
+        nav = true,
+        z = true,
+        g = true,
+      },
+    },
     preset = 'modern',
     win = {
       no_overlap = false,
@@ -19,10 +37,10 @@ return {
       align = 'left',
     },
     icons = {
-      mappings = false,
-      breadcrumb = '',
-      separator = '',
-      group = '',
+      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+      separator = "➜", -- symbol used between a key and it's label
+      group = "+", -- symbol prepended to a group
     },
+    show_help = true,
   },
 }
