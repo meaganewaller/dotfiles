35d85805fbbfbecf9a36f7e5f0310fa4921d9e33
Meagan Waller <meagan.waller@gusto.com>
2024-10-18 11:35:15 -0400

    Add empty reminders icon and label

--- os/macos/sketchybar/plugins/reminders.sh
+++ os/macos/sketchybar/plugins/reminders.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-REMINDERS_COUNT=$(osascript -l JavaScript -e "Application('Reminders').lists.byName('📥 Inbox').reminders.whose({completed: false}).name().length")
+REMINDERS_COUNT=$(osascript -l JavaScript -e "Application('Reminders').lists.byName('Inbox').reminders.whose({completed: false}).name().length")
 
 if [[ $REMINDERS_COUNT -gt 0 ]]; then
   sketchybar -m --set reminders icon="󰢌" \
                 --set reminders icon.font.size=18 \
                 --set reminders label="$REMINDERS_COUNT"
 else
-  sketchybar -m --set reminders icon="" \
-                --set reminders label=""
+  sketchybar -m --set reminders icon="󰢌" \
+                --set reminders label="0"
 fi
