4f8a286dcd3249dc8957b39ad482d2dd7bfcde91
Meagan Waller <meagan@meaganwaller.com>
2024-11-08 11:44:26 -0500

    Add useful nvim plugins

--- editors/nvim/lua/plugins/treesitter.lua
+++ editors/nvim/lua/plugins/treesitter.lua
@@ -10,6 +10,14 @@ return {
           enable = true,
           throttle = true,
           max_lines = 0,
+          min_window_height = 0,
+          line_numbers = true,
+          multiline_threshold = 20,
+          trim_scope = 'outer',
+          mode = 'cursor',
+          separator = nil,
+          zindex = 20,
+          on_attach = nil,
           patterns = {
             default = {
               'class',
