35a717759961ff540a377e0ffc77477e269e0887
Meagan Waller <meagan@meaganwaller.com>
2024-11-08 11:45:10 -0500

    Add openssl to the fish path

--- shells/fish/conf.d/000_path.fish
+++ shells/fish/conf.d/000_path.fish
@@ -1,1 +1,7 @@
 set -gx fish_user_paths ~/.local/bin $fish_user_paths
+
+fish_add_path /opt/homebrew/opt/openssl@3.0/bin
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/openssl@3.0/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@3.0/include"
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/openssl@3.0/lib/pkgconfig"
