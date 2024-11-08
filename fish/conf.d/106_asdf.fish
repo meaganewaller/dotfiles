afea7cd46dde47a827b039addc4d0de815141db8
Meagan Waller <meagan@meaganwaller.com>
2024-11-08 11:41:57 -0500

    feat: add complete fish configuration

--- fish/conf.d/106_asdf.fish
+++
@@ -1,5 +0,0 @@
-set -l asdf_path $HOME/.asdf/asdf.fish
-
-if test -e "$asdf_path"
-	source "$asdf_path"
-end
--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,43 @@
 if test -e "$XDG_CONFIG_HOME/env/env.fish"
    source "$XDG_CONFIG_HOME/env/env.fish"
 end
+
+set -x PATH /opt/homebrew/bin $PATH
+set -x LANG en_US.UTF-8
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+bind --preset \cC 'cancel-commandline'
+
+if type -q nvm
+  function __nvm_auto --on-variable PWD
+  nvm use --silent 2>/dev/null
+  end
+  __nvm_auto
+end
+
+if type -q pyenv
+  status --is-interactive; and source (pyenv init -|pbsub)
+end
+
+# `ls` → `exa` abbreviation
+# Requires `brew install exa`
+if type -q exa
+  abbr --add -g ls 'exa --long --classify --all --header --git --no-user --tree --level 1'
+end
+ 
+# `cat` → `bat` abbreviation
+# Requires `brew install bat`
+if type -q bat
+  abbr --add -g cat 'bat'
+end
+ 
+# `rm` → `trash` abbreviation (moves files to the trash instead of deleting them)
+# Requires `brew install trash`
+if type -q trash
+  abbr --add -g rm 'trash'
+end
+
+set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+starship init fish | source
+enable_transience
