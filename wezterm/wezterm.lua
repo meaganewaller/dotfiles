f72fcd557a7a6bcbca29a27760c22663e49dcdc4
Meagan Waller <meagan@meaganwaller.com>
2024-11-08 11:41:58 -0500

    feat: add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+
+return {
+	tab_max_width = 16,
+	hide_bar_if_only_one_tab = false,
+	font_size = 16,
+	window_background_opacity = 0.97,
+	font = wezterm.font('Fira Code'),
+	color_scheme = 'Catppuccin Mocha',
+	tab_bar_at_bottom = true,
+}
