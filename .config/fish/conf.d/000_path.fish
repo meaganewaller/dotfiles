7be85bad40612afe492e197ea19bedf8c832e404
Meagan Waller <meaganwaller@Meagans-MacBook-Pro.local>
2024-12-01 11:28:19 -0500

    Starting neovim config over, ugh

--- .config/fish/conf.d/000_path.fish
+++ .config/fish/conf.d/000_path.fish
@@ -2,9 +2,9 @@ set -gx fish_user_paths ~/.local/bin $fish_user_paths
 
 fish_add_path /opt/homebrew/opt/openssl@3.0/bin
 
-set -gx LDFLAGS "-L/opt/homebrew/opt/openssl@3.0/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@3.0/include"
-set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/openssl@3.0/lib/pkgconfig"
+set -gx LDFLAGS "-L/opt/homebrew/opt/openssl@3/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@3/include"
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/openssl@3/lib/pkgconfig"
 
 set -gx PNPM_HOME "$HOME/Library/pnpm"
 
